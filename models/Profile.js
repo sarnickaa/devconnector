56f1b9ae73b93a274b6a9950030ab4edfef9c4a4
sarnickaa <sarnicka.aleks@gmail.com>
2018-09-07 15:26:03 -0400

    add Profile model

---
+++ models/Profile.js
@@ -0,0 +1,125 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// create schema:
+const ProfileSchema = new Schema({
+  // associating user with profile
+  // everything in user schema associated with this user
+  user: {
+    type: Schema.Types.ObjectId,
+    // associates user by id with profile
+    ref: 'users'
+    // the collection referred to
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    // array - will be inputed as csv(comma sep values) - logic will turn into array
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema)
