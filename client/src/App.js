f1f1522bde807fd7d6c6ebf89f3184aaa351469b
sarnickaa <sarnicka.aleks@gmail.com>
2018-09-27 11:48:57 -0400

    set up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+//mimics standard browser - allows you to use back button
 import './App.css';
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+
 
 class App extends Component {
   render() {
     return (
+    <Router>
       <div className="App">
         <Navbar />
-        <Landing />
+        <Route path="/" exact component={ Landing } />
+        <div className="container">
+          <Route path="/register" exact component={ Register } />
+          <Route path="/login" exact component={ Login } />
+        </div>
         <Footer />
       </div>
+    </Router>
     );
   }
 }
