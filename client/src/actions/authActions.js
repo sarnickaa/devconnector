43829922a3ffb097c1bb141c692ed00232fcbdf5
sarnickaa <sarnicka.aleks@gmail.com>
2018-10-24 12:51:40 -0400

    adds logout functionality through redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -11,9 +11,9 @@ import { GET_ERRORS, SET_CURRENT_USER } from './types'
 //REGISTER USER ACTION:
 //IF YOU WANT TO DISPATCH SOMETHING TO A REDUCER MUST RETURN AN OBJECT WITH A TYPE
 export const registerUser = (userData, history) => dispatch => {
-// dispatch is a nested function
-// history is the router-specific this.props.history prop that is being passed to this action when it is invoked in the Register component
-// will allow you to redirect from within an action on the success of the axios call
+  // dispatch is a nested function
+  // history is the router-specific this.props.history prop that is being passed to this action when it is invoked in the Register component
+  // will allow you to redirect from within an action on the success of the axios call
 
   // return {
   //   type: TEST_DISPATCH,
@@ -22,53 +22,65 @@ export const registerUser = (userData, history) => dispatch => {
 
   //thunk middle ware for asynch actions
   axios.post('/api/users/register', userData)
-  .then(res => history.push('/login'))
-  // redirecting from an action - from within an action, cant use this.props.hostory.push('path')
-  .catch(err =>
-  //making an axios call and waiting for response - thus can't just return type/payload - have to call dispatch method to actually pass to reducer
-  //dispatch method accessed through redux-thunk middleware
-    dispatch({
-      type: GET_ERRORS,
-      payload: err.response.data
-    })
-    //dispatching an action type to get errors
-  )
+    .then(res => history.push('/login'))
+    // redirecting from an action - from within an action, cant use this.props.hostory.push('path')
+    .catch(err =>
+      //making an axios call and waiting for response - thus can't just return type/payload - have to call dispatch method to actually pass to reducer
+      //dispatch method accessed through redux-thunk middleware
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+      //dispatching an action type to get errors
+    )
 }
 
 
 // LOGIN USER ACTION - get user token
 export const loginUser = userData => dispatch => {
-axios.post('/api/users/login', userData)
-  .then(res => {
-    // save to local storage
-    const { token } = res.data
-    // set token to local storage
-    localStorage.setItem('jwtToken', token)
-    //local storage only stores strings
-    //set token to auth header
-    setAuthToken(token) // function created in a separate file
+  axios.post('/api/users/login', userData)
+    .then(res => {
+      // save to local storage
+      const { token } = res.data
+      // set token to local storage
+      localStorage.setItem('jwtToken', token)
+      //local storage only stores strings
+      //set token to auth header
+      setAuthToken(token) // function created in a separate file
 
-    // set the user - in auth state have a user object that needs to be filled
-    // right now token is bearer token (long string) which includes user info i.e. name/avatar/etc to decode need the jwtDecode module to extract that info
+      // set the user - in auth state have a user object that needs to be filled
+      // right now token is bearer token (long string) which includes user info i.e. name/avatar/etc to decode need the jwtDecode module to extract that info
 
-    //decode token to get user data
-    const decoded = jwt_decode(token) // will have user data + issued at and exp of token
+      //decode token to get user data
+      const decoded = jwt_decode(token) // will have user data + issued at and exp of token
 
-    // set current user on application state
-    dispatch(setCurrentUser(decoded))
-})
-.catch(err =>
-  dispatch({
-    type: GET_ERRORS,
-    payload: err.response.data
-  }))
+      // set current user on application state
+      dispatch(setCurrentUser(decoded))
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      }))
 }
 
 //set logged in user action
 export const setCurrentUser = decoded => {
-// just dispatching to reducer
-return {
-  type: SET_CURRENT_USER,
-  payload: decoded
+  // just dispatching to reducer
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
 }
+
+//log user out
+export const logoutUser = () => dispatch => {
+//remove token local localStorage
+localStorage.removeItem('jwtToken')
+//remove auth header for future requests
+//in utils folder - setAuthToken - if token is in local storage - it attaches token to header for every request
+setAuthToken(false)
+
+//set current user to an empty object which will also set isAuthenticated to false
+dispatch(setCurrentUser({})) // see reducer - payload is empty object - thus will set isAuthenticated to false - sets back to initial state - loggin out
 }
